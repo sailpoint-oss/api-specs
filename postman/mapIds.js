417a616d175a34247a454077e2c4718c894603ef
philip-ellis-sp <philip.ellis@sailpoint.com>
2022-10-11 14:01:39 -0400

    fixed duplicate issue in script

--- postman/mapIds.js
+++ postman/mapIds.js
@@ -10,21 +10,25 @@ let keyReplacedCount = 0
 const getAllKeys = function(fullKey, hierarchy, replaceKey) {
 
     //search the object for a name and key and add/replace if it does
-    const pair = getNameAndKey(hierarchy)
+    const pair = getNameAndKeyAndMethod(hierarchy)
     if (pair.key) {
+        let newKey = fullKey + '|' + pair.name
+        if (pair.method) {
+            newKey += '|' + pair.method
+        }
         if (replaceKey) {
-            if (allKeys.hasOwnProperty(fullKey + pair.name)) {
-                hierarchy.id = allKeys[fullKey + pair.name]
+            if (allKeys.hasOwnProperty(newKey)) {
+                hierarchy.id = allKeys[newKey]
                 // set the pair key to the original so a match can be found in deeper sets
-                pair.key = allKeys[fullKey + pair.name]
+                pair.key = allKeys[newKey]
                 keyReplacedCount++
             }
         } else {
-            if (!allKeys.hasOwnProperty(fullKey + pair.name)) {
-                allKeys[fullKey + pair.name] = pair.key
+            if (!allKeys.hasOwnProperty(newKey)) {
+                allKeys[newKey] = pair.key
                 keyFoundCount++
             } else {
-                console.log('duplicate key found! name=' + pair.name)
+                console.log('duplicate key found! name=' + newKey)
             }
         }
 
@@ -33,19 +37,22 @@ const getAllKeys = function(fullKey, hierarchy, replaceKey) {
     //continue to search deeper in the object
     for (let key of Object.keys(hierarchy)) {
         if (typeof hierarchy[key] === 'object' && hierarchy[key] !== null) {
-            getAllKeys(fullKey + pair.key + (isNaN(key) ? key : ''), hierarchy[key], replaceKey)
+            getAllKeys(fullKey + '|' + pair.key , hierarchy[key], replaceKey)
         }
     }
 }
 
-const getNameAndKey = function(hierarchy) {
-    let pair = {key: '', name: ''}
+const getNameAndKeyAndMethod = function(hierarchy) {
+    let pair = {key: '', name: '', method: ''}
     for (let key of Object.keys(hierarchy)) {
         if (key === 'id') {
             pair.key = hierarchy[key]
         }
         if (key === 'name') {
             pair.name = hierarchy[key]
+        }
+        if (key === 'request') {
+            pair.method = hierarchy[key].method
         }
     }
     return pair
