d20926eb7ca70634a222d0871eff4950aef12b73
Philip Ellis <philip.ellis@sailpoint.com>
2023-08-31 15:59:15 -0500

    updated script to include different base path for each collection

--- postman-script/modify-collection.js
+++ postman-script/modify-collection.js
@@ -9,6 +9,11 @@ const deleteAuthKey = (obj) => {
         if (key === 'auth') {
         delete obj[key];
         }
+        if (key === 'disabled') {
+          if (obj[key] === false) {
+            obj[key] = true
+          }
+        }
     }
     };
 
@@ -31,7 +36,16 @@ fs.readFile(args[2], 'utf8', (err, data) => {
     deleteAuthKey(jsonObject);
     jsonObject.auth = JSON.parse(fs.readFileSync('postman-script/base-auth.json', 'utf8'));
     jsonObject.event = JSON.parse(fs.readFileSync('postman-script/pre-script.json', 'utf8'));
-    jsonObject.variable = JSON.parse(fs.readFileSync('postman-script/variable.json', 'utf8'));
+    if (args[2].includes("beta")) {
+      jsonObject.variable = JSON.parse(fs.readFileSync('postman-script/variable-beta.json', 'utf8'));
+    } else if (args[2].includes("v3")) {
+      jsonObject.variable = JSON.parse(fs.readFileSync('postman-script/variable-v3.json', 'utf8'));
+    } else if (args[2].includes("v2")) {
+      jsonObject.variable = JSON.parse(fs.readFileSync('postman-script/variable-v2.json', 'utf8'));
+    } else if (args[2].includes("cc")) {
+      jsonObject.variable = JSON.parse(fs.readFileSync('postman-script/variable-cc.json', 'utf8'));
+    }
+    
     
 
     // Write the modified JSON content back to the file
